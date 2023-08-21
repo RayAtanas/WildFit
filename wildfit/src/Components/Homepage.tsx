a2e63906aa443979caf52ed5a65e5d83e93ad45d
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-08-21 19:44:39 +0300

    side bar creation

--- wildfit/src/Components/Homepage.tsx
+++ wildfit/src/Components/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../CSS/Homepage.css"
+import { Row } from "react-bootstrap";
 function Homepage() {
 
     return (
@@ -13,19 +14,33 @@ function Homepage() {
                     </div>
                     <div className="col-5">
                         <nav>
-                            <button className="col col-lg-2" >New Arrival</button>
-                            <button className="col col-lg-2">Wishlist</button>
-                            <button className="col col-lg-2">Cart</button>
-                            <input className="searchbar col col-lg-2" placeholder="Search"/>
+                            <button className="header-btn col col-lg-2" >New Arrival</button>
+                            <button className="header-btn col col-lg-2">Wishlist</button>
+                            <button className="header-btn col col-lg-2">Cart</button>
+                            <input className="searchbar col col-lg-2" placeholder="Search" />
                         </nav>
                     </div>
 
                 </div>
-                
+
             </div>
 
             <div className="body container-fluid">
 
+                <div className="sidebar col-2 row justify-content-start">
+
+                    <div className="side_button sidebar row justify-content-start row-col-1">
+
+                        <div className="categories">
+                            <h3>Categories</h3>
+                        </div>
+
+                        <button className="side_button">New</button>
+                        <button className="side_button">Best Sellers</button>
+                        <button className="side_button">Hoodies</button>
+                    </div>
+
+                </div>
             </div>
 
 
