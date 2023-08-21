e415d93ccc9fed2d9e2f9665be0bc65f0c8930c9
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-08-21 17:19:23 +0300

    installed bootstrap

--- wildfit/src/index.tsx
+++ wildfit/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Homepage from './Components/Homepage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
