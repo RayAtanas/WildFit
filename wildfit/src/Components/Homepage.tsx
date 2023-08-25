08635bc5b04b7c0a814660d9416461e4e8d8b76a
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-08-26 00:53:48 +0300

    removed the side bar and header from the home page

--- wildfit/src/Components/Homepage.tsx
+++ wildfit/src/Components/Homepage.tsx
@@ -1,49 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "../CSS/Homepage.css"
 import { Row } from "react-bootstrap";
+import "../Props/Clothes"
+import Clothes from "../Props/Clothes";
 function Homepage() {
 
     return (
         <div>
+            {/* Header */}
+            
 
-            <div className=" header container-fluid">
-                <div className="row justify-content-end">
-                    <div className="title col-7">
-                        <h1>Wildfit</h1>
-                    </div>
-                    <div className="col-5">
-                        <nav>
-                            <button className="header-btn col col-lg-2" >New Arrival</button>
-                            <button className="header-btn col col-lg-2">Wishlist</button>
-                            <button className="header-btn col col-lg-2">Cart</button>
-                            <input className="searchbar col col-lg-2" placeholder="Search" />
-                        </nav>
-                    </div>
-
+            {/* main */}
+            <div>
+                <div>
+                    {/* sideBar */}
                 </div>
-
+                <div>
+                    {/* Content */}
+                </div>
             </div>
 
-            <div className="body container-fluid">
+            {/* footer */}
 
-                <div className="sidebar col-2 row justify-content-start">
 
-                    <div className="side_button sidebar row justify-content-start row-col-1">
+            {/* to be moved */}
+            <div className="body d-flex container-fluid">
+
+                {/* sideBr */}
+                <div className="sidebar col-2 row justify-content-start d-inline me-5">
+
+                    <div className="side_button row justify-content-start p-2- m-2">
 
                         <div className="categories">
                             <h3>Categories</h3>
                         </div>
 
-                        <button className="side_button">New</button>
-                        <button className="side_button">Best Sellers</button>
-                        <button className="side_button">Hoodies</button>
+                        <a className="side_button">New</a>
+                        <a className="side_button">Best Sellers</a>
+                        <a className="side_button">Hoodies</a>
+
+                    </div>
+
+                </div>
+                {/* content */}
+                <div className="col-10 row p-3 m-auto content">
+                    <div className="d-flex justify-content-around border border-5 ">
+                        
+                        <Clothes className="col-4 mx-2 p-3" name="Jacket" img="" alt="test1" price={50} type="winter" size="large" />
+                        <Clothes className="col-4 mx-2 p-3" name="short" img="" alt="test2" price={30} type="summer" size="Small" />
+                        <Clothes className="col-4 mx-2 p-3" name="joseph" img="" alt="" price={100} type="charmourta" size="xtiziblackhole" />
+
                     </div>
 
                 </div>
             </div>
-
-
         </div>
     )
 
