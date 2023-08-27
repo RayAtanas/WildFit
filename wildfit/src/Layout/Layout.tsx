968f08818dd1c08e8486b75fd3224d25423d010f
RAY-PC\Ray Atanas <rayatanas@hotmail.com>
2023-08-27 22:53:32 +0300

    added the Layout file

--- wildfit/src/Layout/Layout.tsx
+++ wildfit/src/Layout/Layout.tsx
@@ -1,29 +1,45 @@
 
 
 
-function Layout(){
-return(
-    <>
-    <div className=" header container-fluid">
+function Layout() {
+    return (
+        <>
+            <div className=" header container-fluid">
 
-<div className="row justify-content-end">
-    <div className="title col-7">
-        <h1>Wildfit</h1>
-    </div>
-    <div className="col-5">
-        <nav>
-            <button className="header-btn col col-lg-2" >New Arrival</button>
-            <button className="header-btn col col-lg-2">Wishlist</button>
-            <button className="header-btn col col-lg-2">Cart</button>
-            <input className="searchbar col col-lg-2" placeholder="Search" />
-        </nav>
-    </div>
-</div>
+                <div className="row justify-content-end">
+                    <div className="title col-7">
+                        <h1>Wildfit</h1>
+                    </div>
+                    <div className="col-5">
+                        <nav>
+                            <button className="header-btn col col-lg-2" >New Arrival</button>
+                            <button className="header-btn col col-lg-2">Wishlist</button>
+                            <button className="header-btn col col-lg-2">Cart</button>
+                            <input className="searchbar col col-lg-2" placeholder="Search" />
+                        </nav>
+                    </div>
+                </div>
 
-</div>
-    
-    
-    
-    </>
-)
-}export default Layout
+            </div>
+            
+
+            <div className="sidebar col-2 row justify-content-start d-inline me-5">
+
+                    <div className="side_button row justify-content-start p-2- m-2">
+
+                        <div className="categories">
+                            <h3>Categories</h3>
+                        </div>
+
+                        <a className="side_button">New</a>
+                        <a className="side_button">Best Sellers</a>
+                        <a className="side_button">Hoodies</a>
+
+                    </div>
+
+                </div>
+
+
+        </>
+    )
+} export default Layout
